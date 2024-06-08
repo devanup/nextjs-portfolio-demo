9e932937e73b9a61a5cf4ae3a7e90a5397617be7
Anup <anupthedev@gmail.com>
2024-06-08 16:03:20 -0400

    Completed the tutorial

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,26 @@
 import Hero from './components/Hero';
 import { FloatingNav } from './components/ui/FloatingNav';
+import Grid from './components/ui/Grid';
+import RecentProjects from './components/RecentProjects';
+import { navItems } from '@/data';
+import Clients from './components/Clients';
+import Experience from './components/Experience';
+import Approach from './components/Approach';
+import Footer from './components/Footer';
 
 export default function Home() {
 	return (
 		// <main className='relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5'>
-		<main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
+		<main className='relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip'>
 			<div className='max-w-7xl w-full'>
-				<FloatingNav
-					navItems={[
-						{
-							name: 'Home',
-							link: '/',
-						},
-						{
-							name: 'About',
-							link: '#about',
-						},
-						{
-							name: 'Projects',
-							link: '#projects',
-						},
-						{
-							name: 'Contact',
-							link: '#contact',
-						},
-					]}
-				/>
+				<FloatingNav navItems={navItems} />
 				<Hero />
+				<Grid />
+				<RecentProjects />
+				<Clients />
+				<Experience />
+				<Approach />
+				<Footer />
 			</div>
 		</main>
 	);
